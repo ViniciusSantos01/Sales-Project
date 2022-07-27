8bf2f88a1c2c0cbf36ac4849377a5c3938e968c1
ViniciusSantos01 <vini.santos020@outlook.com>
2022-07-27 09:28:09 +0200

    commit inicial

---
+++ src/main/java/io/github/ViniciusSantos01/SalesApplication.java
@@ -0,0 +1,13 @@
+package io.github.ViniciusSantos01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SalesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesApplication.class, args);
+    }
+}
