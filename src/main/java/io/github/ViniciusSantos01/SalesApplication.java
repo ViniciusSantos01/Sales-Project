15d916ec1656d54fb2b4df7206cbcca842ec6450
ViniciusSantos01 <vini.santos020@outlook.com>
2022-09-13 17:43:00 +0200

    commit inicial

--- src/main/java/io/github/ViniciusSantos01/SalesApplication.java
+++ src/main/java/io/github/ViniciusSantos01/SalesApplication.java
@@ -6,11 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
-public class SalesApplication {
+public class SalesApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
        SpringApplication.run(SalesApplication.class, args);
